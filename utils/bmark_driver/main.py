33f0a18ab1e93968ba7a7f020a0feb855d222d3b
Yuta Saito <kateinoigakukun@gmail.com>
2022-07-07 18:35:52 +0000

    count actual object size

--- utils/bmark_driver/main.py
+++ utils/bmark_driver/main.py
@@ -12,13 +12,16 @@ class BenchmarkCase:
         self.bitcode_path = bitcode_path
 
     def plan(self, options):
-        cmd = [options.opttool, "--filetype=obj", self.bitcode_path]
+        opt_cmd = [options.opttool, self.bitcode_path]
         if options.pass_plugin:
-            cmd += ["--load", options.pass_plugin,
-                    "--load-pass-plugin", options.pass_plugin]
+            opt_cmd += ["--load", options.pass_plugin,
+                        "--load-pass-plugin", options.pass_plugin]
         if options.Xopt:
-            cmd += options.Xopt
-        return cmd
+            opt_cmd += options.Xopt
+
+        llc_cmd = [options.llctool, "-filetype=obj", "-", "-o", "-"]
+        return [opt_cmd, llc_cmd]
+
 
 class ConsoleReporter:
     def __init__(self, options):
@@ -27,6 +30,7 @@ class ConsoleReporter:
     def report(self, case, size, returncode, time, stderr):
         print("{} {} {} {}".format(case.bitcode_path, size, returncode, time))
         pass
+
 
 class SQLiteReporter:
     def __init__(self, options):
@@ -54,6 +58,7 @@ class SQLiteReporter:
         """, (case.bitcode_path, size, returncode, time, stderr))
         self.db.commit()
 
+
 class BenchmarkDriver:
     def __init__(self, cases):
         self.cases = cases
@@ -69,37 +74,62 @@ class BenchmarkDriver:
         for case in self.cases:
             self.run_case(case, reporter, options)
 
+    def format_command(self, cmd):
+        return " ".join(map(lambda x: "'" + x + "'", cmd))
+
     def run_case(self, case: BenchmarkCase, reporter: ConsoleReporter, options):
-        cmd = case.plan(options)
+        cmds = case.plan(options)
         start = time.perf_counter()
         if options.verbose:
-            print(" ".join(map(lambda x: "'" + x + "'", cmd)))
-        proc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            print(" | ".join(map(lambda cmd: self.format_command(cmd), cmds)))
+
+        # create a pipeline through stdout/stdin
+        procs = []
+        last_proc = None
+        for cmd in cmds:
+            stdin = last_proc.stdout if last_proc else None
+            last_proc = subprocess.Popen(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin)
+            procs.append(last_proc)
+
+        for proc in reversed(procs):
+            proc.wait()
+
+        last_proc = procs[-1]
+        output = last_proc.stdout.read()
         end = time.perf_counter()
-        reporter.report(case, size=len(proc.stdout),
-                        returncode=proc.returncode,
-                        time=end - start, stderr=proc.stderr)
+        reporter.report(case, size=len(output),
+                        returncode=last_proc.returncode,
+                        time=end - start, stderr=last_proc.stderr)
+
+
 def make_reporter(options):
     if options.db_path:
         return SQLiteReporter(options)
     else:
         return ConsoleReporter(options)
 
+
 def main():
     parser = argparse.ArgumentParser(description="""
 Compare code sizes of differently optimized code from the same source code.""")
     parser.add_argument("--opttool", default="opt", help="Path to opt tool")
+    parser.add_argument("--llctool", default="llc", help="Path to llc tool")
     parser.add_argument("--pass-plugin", help="Path to LLVM pass plugin")
-    parser.add_argument("-Xopt", action='append', help="Extra arguments to pass to opt")
-    parser.add_argument("--suite-path", required=True, help="Path to benchmark suite directory")
+    parser.add_argument("-Xopt", action='append',
+                        help="Extra arguments to pass to opt")
+    parser.add_argument("--suite-path", required=True,
+                        help="Path to benchmark suite directory")
     parser.add_argument("--db-path", help="Path to SQLite database")
-    parser.add_argument("--verbose", action='store_true', help="Print extra information")
+    parser.add_argument("--verbose", action='store_true',
+                        help="Print extra information")
 
     options = parser.parse_args()
     cases = list(BenchmarkDriver.find_cases(options.suite_path))
     reporter = make_reporter(options)
     driver = BenchmarkDriver(cases)
     driver.run(options, reporter)
+
 
 if __name__ == "__main__":
     main()
